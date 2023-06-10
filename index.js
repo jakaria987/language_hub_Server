a23e2de4189ffd1378ad3ff56780cd9fa09fada9
Md Jakaria <jakhossain999@gmail.com>
2023-06-10 06:34:51 +0600

    add delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
-require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gpvu0c0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,7 +43,7 @@ async function run() {
 
 
     // cart collection
-    
+
     app.get('/carts', async (req, res) => {
         const email = req.query.email;
         if(!email){
@@ -58,6 +58,13 @@ async function run() {
         console.log(item);
         const result = await cartCollection.insertOne(item);
         res.send(result);
+    })
+    app.delete('/carts/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await cartCollection.deleteOne(query);
+        res.send(result);
+
     })
 
     // Send a ping to confirm a successful connection
