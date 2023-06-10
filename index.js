1d9df75c8c206d3db7b6b327d39dd8b9649f8d10
Md Jakaria <jakhossain999@gmail.com>
2023-06-10 22:14:49 +0600

    verify jwt

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-require('dotenv').config()
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization){
+      return res.status(401).send({error : true, message : "Unauthorized access"});
+    }
+    // bearer token
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if(err){
+        return res.status(401).send({error : true, message : "Unauthorized access"});
+      }
+      req.decoded = decoded;
+      next();
+    })
+  } 
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -31,6 +48,15 @@ async function run() {
     const classesCollection = client.db('languageDb').collection('classes');
     const instructorsCollection = client.db('languageDb').collection('instructors');
     const cartCollection = client.db('languageDb').collection('carts');
+
+
+    app.post('/jwt', (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '7d'})
+        res.send({token})
+    })
+
+
 
     // users related apis
     app.get('/users', async(req, res) => {
@@ -89,11 +115,17 @@ async function run() {
 
     // cart collection
 
-    app.get('/carts', async (req, res) => {
+    app.get('/carts', verifyJWT, async (req, res) => {
         const email = req.query.email;
         if(!email){
             res.send([]);
         }
+
+        const decodedEmail = req.decoded.email;
+        if(email !== decodedEmail){
+        return res.status(403).send({error : true, message : "Forbidden access"});
+      }
+
         const query = {email : email}
         const result = await cartCollection.find(query).toArray();
         res.send(result);
