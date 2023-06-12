3b982b72e34dbe416ca676989f8424c26381cafc
Md Jakaria <jakhossain999@gmail.com>
2023-06-12 08:32:03 +0600

    delete user from admin panel funtionalit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -48,6 +49,7 @@ async function run() {
     const classesCollection = client.db('languageDb').collection('classes');
     const instructorsCollection = client.db('languageDb').collection('instructors');
     const cartCollection = client.db('languageDb').collection('carts');
+    const paymentCollection = client.db('languageDb').collection('payments');
 
 
     app.post('/jwt', (req, res) => {
@@ -139,6 +141,13 @@ async function run() {
 
           const result = await usersCollection.updateOne(query, updateDoc);
           res.send(result);
+    })
+    app.delete('/users/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await usersCollection.deleteOne(query);
+        res.send(result);
+
     })
 
 
@@ -243,6 +252,32 @@ async function run() {
         res.send(result);
 
     })
+
+
+
+    // for payment related
+
+    app.post('/create-payment-intent', verifyJWT, async(req, res)=> {
+        const {price} = req.body;
+        const amount = parseInt(price*100);
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'USD',
+          payment_method_types: ["card"]
+        });
+        res.send({
+          clientSecret: paymentIntent.client_secret
+        })
+      })
+      app.post('/payments', verifyJWT, async(req, res) => {
+        const payment = req.body;
+        const insertResult = await paymentCollection.insertOne(payment);
+  
+        const query = {_id : { $in: payment.cartItems.map(id => new ObjectId(id)) }}
+        const deleteResult = await cartCollection.deleteMany(query)
+  
+        res.send({insertResult, deleteResult});
+      })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
